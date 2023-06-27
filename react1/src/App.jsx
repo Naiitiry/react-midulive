fe8ceba2354d47e227749334f09eee4ceabf05f4
Naiitiry <you@example.com>
2023-06-27 14:07:34 -0300

    clases de practica con Midulive

---
+++ react1/src/App.jsx
@@ -0,0 +1,20 @@
+import '../src/App.css'
+
+export function App(){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/kikobeats" alt="Mi Avatar" srcset="" />
+                <div>
+                    <strong>Roman Danchuk</strong>
+                    <span>@romanDanchuk</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
---
+++ react1/src/TwitterFollowCard.jsx
@@ -0,0 +1,18 @@
+export function TwitterFollowCard(){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/kikobeats" alt="Mi Avatar" srcset="" />
+                <div>
+                    <strong>Roman Danchuk</strong>
+                    <span>@romanDanchuk</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
